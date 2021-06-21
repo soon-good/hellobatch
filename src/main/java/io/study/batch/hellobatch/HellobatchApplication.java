0e0989ef83aee1afbd02c4363d0bbb783ae72bc3
gosgjung <playmymind@daum.net>
2021-06-21 11:57:41 +0900

    fisrt commit

---
+++ src/main/java/io/study/batch/hellobatch/HellobatchApplication.java
@@ -0,0 +1,13 @@
+package io.study.batch.hellobatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellobatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellobatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/study/batch/hellobatch/HellobatchApplicationTests.java
@@ -0,0 +1,13 @@
+package io.study.batch.hellobatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellobatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
